7021cfc0bbd373f2075b41a34f86ec95374c5861
swannyPeng <ph1994wh@gmail>
2019-10-08 16:23:33 +0800

    fix compile_module

--- cmake/CompileModule.cmake
+++ cmake/CompileModule.cmake
@@ -7,26 +7,24 @@
 #
 macro(COMPILE_MODULE module_name)
 
-  if(module_name STREQUAL "core")
+  if(${module_name} STREQUAL "core")
     file(GLOB SOURCES_ELIB_${module_name} 
           "${ELIB_SOURCE_DIR}/easylib/*.cc" "${ELIB_SOURCE_DIR}/easylib/*.cpp" "${ELIB_SOURCE_DIR}/easylib/*.cxx")
   else()
     file(GLOB SOURCES_ELIB_${module_name}
     "${ELIB_SOURCE_DIR}/${module_dir}/*.cpp" "${ELIB_SOURCE_DIR}/${module_dir}/*.cc" "${ELIB_SOURCE_DIR}/${module_dir}/*.cxx")
+    
   endif()
   
   set(elib_module_name "elib_${module_name}")
-
   string(TOUPPER ${module_name} SCOPE_NAME)
-  if(NOT ${SOURCES_ELIB_${module_name}})
+  if(NOT ${SOURCES_ELIB_${module_name}} STREQUAL "")
     if(USE_STATIC_LIBRARY )
       set(ELIB_SCOPE_WITH_${SCOPE_NAME} PUBLIC)
       add_library(${elib_module_name} STATIC ${SOURCES_ELIB_${module_name}})
-
       if(MSVC)
         target_compile_options(${elib_module_name} PRIVATE /w) # disable all warnings (not ideal but...)
       endif()
-
     else() 
       set(ELIB_SCOPE_WITH_${SCOPE_NAME} INTERFACE)
       add_library(${elib_module_name} INTERFACE)
@@ -34,6 +32,12 @@ macro(COMPILE_MODULE module_name)
   else()
     set(ELIB_SCOPE_WITH_${SCOPE_NAME} INTERFACE)
     add_library(${elib_module_name} INTERFACE)
+  endif()
+
+  if(${module_name} STREQUAL "core")
+    target_link_libraries(${elib_module_name} ELIB_SCOPE_WITH_${SCOPE_NAME} elib_common) 
+  else()
+    target_link_libraries(${elib_module_name} ELIB_SCOPE_WITH_${SCOPE_NAME} elib_core) 
   endif()
 
   message(STATUS "Creating target: elib::${module_name} (${elib_module_name})")
